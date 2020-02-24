d56e5639c3809b6668e3b5180b7da5c2531a1572
Naveen Arora <naveenarorakrnl@gmail.com>
2020-02-24 16:03:28 +0530

    GET method - python requests

---
+++ request.py
@@ -0,0 +1,14 @@
+import requests
+
+# Making a HEAD request
+r = requests.get('https://httpbin.org/get', data={'key':'value'})
+
+#check status code for response recieved
+# success code - 200
+print(r)
+
+# print headers of request
+print(r.headers)
+
+# checking if request contains any content
+print(r.content)
