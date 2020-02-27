5ca735359c33a8339c9cf225ecf29e16fadf21b6
Naveen Arora <naveenarorakrnl@gmail.com>
2020-02-27 13:41:04 +0530

    response content - Python requests

--- request.py
+++ request.py
@@ -1,14 +1,7 @@
 import requests
 
 # Making a put request
-r = requests.put('https://httpbin.org/put', data={'key':'value'})
-
-#check status code for response recieved
-# success code - 200
-print(r)
-
-# print headers of request
-print(r.headers)
+response = requests.get('https://api.github.com')
 
 # checking if request contains any content
-print(r.content)
+print(response.content)
